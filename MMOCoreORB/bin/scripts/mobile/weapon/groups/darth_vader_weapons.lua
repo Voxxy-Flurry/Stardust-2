28e5a58292de8b423613d8d77139996c68967f02
Levarrishawk <levarrishawk@gmail.com>
2024-08-18 21:46:15 -0400

    [iteration]

--- MMOCoreORB/bin/scripts/mobile/weapon/groups/darth_vader_weapons.lua
+++ MMOCoreORB/bin/scripts/mobile/weapon/groups/darth_vader_weapons.lua
@@ -1,5 +1,5 @@
 darth_vader_weapons = {
-	"object/weapon/melee/sword/sword_lightsaber_vader.iff"
+	"object/weapon/melee/2h_sword/crafted_saber/sword_lightsaber_two_handed_s13_gen4.iff"
 }
 
 addWeapon("darth_vader_weapons", darth_vader_weapons)
